435863ebd172a5643b5daf603f9f59de0d421ddc
kontakt@nailit.pl <kontakt@nailit.pl>
2023-11-09 12:22:16 +0100

    upgrading to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import {I18nextProvider} from "react-i18next";
 import i18next from "i18next";
@@ -21,4 +21,7 @@ i18next.init({
     },
 });
 
-ReactDOM.render(<I18nextProvider i18n={i18next}><App /></I18nextProvider>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<I18nextProvider i18n={i18next}><App /></I18nextProvider>);
